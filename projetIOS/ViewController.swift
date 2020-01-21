627a0b53e6e0d62475294aed3f24bdbf2e17e006
jerome <jerome.gerard02@outlook.fr>
2020-01-21 10:27:24 +0100

    Initial Commit

---
+++ projetIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projetIOS
+//
+//  Created by etudiant on 21/01/2020.
+//  Copyright © 2020 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
